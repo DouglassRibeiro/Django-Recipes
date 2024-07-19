07e0d13cf5c201428e5f39d9b3e7f5279a299eb8
Douglas Ribeiro <douglasribeirodarosa@gmail.com>
2024-07-19 16:45:16 -0300

    Exemplo de Template

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'global/home.html', context={
+        'name': 'Douglas Ribeiro',
+    })
+
+def contato(request):
+    return render(request, 'temp/temp.html')
+
+def sobre(request):
+    return HttpResponse('sobre')
