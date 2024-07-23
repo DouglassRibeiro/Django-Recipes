2d2fb29b7136233e9717c7e7edee93fbb8437c7a
Douglas Ribeiro <douglasribeirodarosa@gmail.com>
2024-07-22 21:00:51 -0300

    iniciando a moldar o site

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'global/home.html', context={
         'name': 'Douglas Ribeiro',
     })
-
-def contato(request):
-    return render(request, 'temp/temp.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
