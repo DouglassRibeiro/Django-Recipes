ba8ec725a7e838c5391c4b70989fc78114e0d0f2
Douglas Ribeiro <douglasribeirodarosa@gmail.com>
2024-07-30 22:14:06 -0300

    Injetando dados na lista da view Home - "recipe" - "recipes"

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+from recipes.utils.recipes.factory import make_recipe
+
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Douglas Ribeiro',
+        'recipes': [make_recipe() for _ in range (10)],
     })
     
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Douglas Ribeiro',
+        'recipe': make_recipe(),
     })
