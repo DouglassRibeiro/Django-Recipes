480357b5a7f1c31b4f4b6b48db87f6ddc10e2be8
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2025-03-11 15:57:26 -0300

    68. Renomeando um projeto Django inteiro

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dougl\OneDrive\Documentos\Udemy\Curso de Django Web Framework e Django Rest Framework\curso-jango-projeto1\venv
+set VIRTUAL_ENV=C:\Users\dougl\OneDrive\Documentos\Udemy\Curso de Django Web Framework e Django Rest Framework\curso-jango-project1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
