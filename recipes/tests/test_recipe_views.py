40eefc4b90212dab7a42898e4ae552273baf2756
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2025-09-02 16:31:15 -0300

    81. Testando o context e content da response no template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -46,7 +46,12 @@ class RecipeViewsTest(TestCase):
             is_published='True',
         )
         response = self.client.get(reverse('recipes:home'))
-        ...
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_views_function_is_correct(self):
         response = self.client.get(
